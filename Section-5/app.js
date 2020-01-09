31d713904983e3f38f24ac1f56880ef3de03c73e
awguitar86 <awguitar86@gmail.com>
2020-01-09 14:07:58 -0700

    Completed section 5 on Udemy. Added folders for css, html, and routes.

--- Section-5/app.js
+++ Section-5/app.js
@@ -1,22 +1,24 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path')
+const rootDir = require('./util/path')
 const port = 3000
 
 const app = express()
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(rootDir, 'public')))
 
-app.use('/add-product', (req, res) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
-})
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
 
-app.use('/product', (req, res) => {
-  console.log(req.body)
-  res.redirect('/')
-})
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
 
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>')
 })
 
 app.listen(port, () => {
